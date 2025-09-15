9a6bcddb6b14934008b0de16552fcce7ef4673ea
Anushka Rodrigo <anushka.rodrigo14@gmail.com>
2025-09-15 17:39:59 +0530

    Created R code for calculations of the 4 individual sections in the research

---
+++ Challenges in physical learning.R
@@ -0,0 +1,33 @@
+#install package if not installed
+install.packages("readxl")
+
+library(readxl)
+
+# Read the data
+df <- read_excel(
+  "Questionaire_calculations.xlsx",
+  sheet = "Initial Response Calc",
+  range="K18:N28"
+)
+
+# Define weights (order must match columns)
+weights <- c(4, 3, 2, 1)
+
+# Ensure all data is numeric
+df_numeric <- as.data.frame(lapply(df, as.numeric))
+
+# Calculate weighted mean for each row
+weighted_means <- apply(df_numeric, 1, function(x) {
+  sum(x * weights) / sum(x)  # divide by total respondents
+})
+
+#round to 2 decimal places
+weighted_means_rounded <- round(weighted_means, 2)
+
+# View results, printed in each line
+cat(weighted_means_rounded, sep = "\n")
+
+#grand mean calculation
+grand_mean <- round(mean(unlist(weighted_means)), 2)
+cat("Grand mean for challenges in physical learning is", grand_mean) 
+
