f22784ff96a64d45e64376603ae0c0adad8b8505
Anushka Rodrigo <anushka.rodrigo14@gmail.com>
2025-09-16 18:54:37 +0530

    Fixed variable names

--- Final mean and SD of online learning.R
+++ Final mean and SD of online learning.R
@@ -5,29 +5,29 @@ setwd("C:/Users/USER/Downloads/SDI grp project") #put your file location
 library(readxl)
 
 # Read the data
-df <- read_excel(
+dfOnline <- read_excel(
   "Sample_Responses.xlsx",
   sheet = "Online learning impact",
   range="A1:J44"
 )
 
 # Add row totals
-df$RowTotal <- rowSums(df[ , ], na.rm = TRUE)
+dfOnline$RowTotal <- rowSums(dfOnline[ , ], na.rm = TRUE)
 
 # Calculate the mean of the last column
-mean_last_col <- mean(df[[ncol(df)]], na.rm = TRUE)
+mean_last_col <- mean(dfOnline[[ncol(dfOnline)]], na.rm = TRUE)
 meanOnline <- round(mean_last_col, 2)
 
 #add new row to calc the SD
-df$SquaredDeviation <- (df$RowTotal-meanOnline)^2
-df$SquaredDeviation <- round(df$SquaredDeviation,4)
+dfOnline$SquaredDeviation <- (dfOnline$RowTotal-meanOnline)^2
+dfOnline$SquaredDeviation <- round(dfOnline$SquaredDeviation,4)
 
 #SD calc
-sd_sum <- sum(df[[ncol(df)]], na.rm = TRUE)
-variance <- sd_sum/(nrow(df)-1)
-sd <- sqrt(variance)
-sd <- round(sd,2)
+sd_sum <- sum(dfOnline[[ncol(dfOnline)]], na.rm = TRUE)
+variance <- sd_sum/(nrow(dfOnline)-1)
+sdOnline <- sqrt(variance)
+sdOnline <- round(sdOnline,2)
 
 #print final calculations
 cat("Final Mean for Online Learning sample: ", meanOnline, "\n")
-cat("Final Standard Deviation for Online Learning sample: ", sd)
+cat("Final Standard Deviation for Online Learning sample: ", sdOnline)
--- Final mean and SD of physical learning.R
+++ Final mean and SD of physical learning.R
@@ -5,29 +5,29 @@ setwd("C:/Users/USER/Downloads/SDI grp project") #put your file location
 library(readxl)
 
 # Read the data
-df <- read_excel(
+dfPhysical <- read_excel(
   "Sample_Responses.xlsx",
   sheet = "Physical learning impact",
   range="A1:J39"
 )
 
 # Add row totals
-df$RowTotal <- rowSums(df[ , ], na.rm = TRUE)
+dfPhysical$RowTotal <- rowSums(dfPhysical[ , ], na.rm = TRUE)
 
 # Calculate the mean of the last column
-mean_last_col <- mean(df[[ncol(df)]], na.rm = TRUE)
+mean_last_col <- mean(dfPhysical[[ncol(dfPhysical)]], na.rm = TRUE)
 meanPhysical <- round(mean_last_col, 2)
 
 #add new row to calc the SD
-df$SquaredDeviation <- (df$RowTotal-meanPhysical)^2
-df$SquaredDeviation <- round(df$SquaredDeviation,4)
+dfPhysical$SquaredDeviation <- (dfPhysical$RowTotal-meanPhysical)^2
+dfPhysical$SquaredDeviation <- round(dfPhysical$SquaredDeviation,4)
 
 #SD calc
-sd_sum <- sum(df[[ncol(df)]], na.rm = TRUE)
-variance <- sd_sum/(nrow(df)-1)
-sd <- sqrt(variance)
-sd <- round(sd,2)
+sd_sum <- sum(dfPhysical[[ncol(dfPhysical)]], na.rm = TRUE)
+variance <- sd_sum/(nrow(dfPhysical)-1)
+sdPhysical <- sqrt(variance)
+sdPhysical <- round(sdPhysical,2)
 
 #print final calculations
-cat("Final Mean for Physical Learning sample: ", meanOnline, "\n")
-cat("Final Standard Deviation for Physical Learning sample: ", sd)
+cat("Final Mean for Physical Learning sample: ", meanPhysical, "\n")
+cat("Final Standard Deviation for Physical Learning sample: ", sdPhysical)
